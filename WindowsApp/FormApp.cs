2a0d3434af289971f8a05940f792ec10c27ea6d5
tiago-saldanha <tiago.avila.saldanha@gmail.com>
2024-10-16 00:51:06 -0300

    refactoring-to-others-projects

---
+++ WindowsApp/FormApp.cs
@@ -0,0 +1,36 @@
+using YTDownload.Application.Commands;
+using YTDownload.Application.Interfaces;
+
+namespace WindowsApp
+{
+    public partial class FormApp : Form
+    {
+        private readonly IYoutubeService _service;
+
+        public FormApp(IYoutubeService service)
+        {
+            InitializeComponent();
+            _service = service;
+        }
+
+        private async void buttonDownload_Click(object sender, EventArgs e)
+        {
+            var command = new DownloadVideoCommand { Url = textBoxUrlVideo.Text, Mp4 = false, Resolutiuon = "1080p" };
+            var result = string.Empty;
+            try
+            {
+                textBoxOutput.Text = string.Empty;
+                result = await _service.DownloadVideo(command);
+                
+            }
+            catch
+            {
+            }
+            finally
+            {
+                textBoxOutput.Text = result;
+                textBoxUrlVideo.Text = string.Empty;
+            }
+        }
+    }
+}
