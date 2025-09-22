54b2d409490a43ea1e1b4f99946f229aaf3ab907
tiago-saldanha-sige <tiago.saldanha@sigeatende.com.br>
2025-09-21 21:31:00 -0300

    add-cors-to-angular-dev

--- YTDownload/Program.cs
+++ YTDownload/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ProvideServices();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("angular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,9 +24,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("angular");
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
